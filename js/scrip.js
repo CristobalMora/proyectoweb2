af351443441098fb5289530f35e9c4c560d8e403
CristobalMora <128197752+CristobalMora@users.noreply.github.com>
2023-04-05 14:29:20 -0400

    creacion de buscador no funcional aun

---
+++ js/scrip.js
@@ -0,0 +1,10 @@
+//buscador de contenido
+
+//declarando variables
+bars_serach = document.getElementById("ctn-bars-search");
+cover_ctn_search= document.getElementById("cover-ctn-search");
+inputSearch= document.getElementById("inpoutSearch");
+box_search= document.getElementById("box-search");
+
+//funcion para mostar el buscador
+
